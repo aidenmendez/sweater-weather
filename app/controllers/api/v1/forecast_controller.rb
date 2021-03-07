7ee4b3099406c4bb87d2b345806084f26b62b326
Aiden Mendez <aidenmendez@gmail.com>
2021-03-07 13:34:20 -0700

    Add rescue clause to index

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,8 +2,7 @@ class Api::V1::ForecastController < ApplicationController
   def index
     begin
       location_coordinates = GeocodeFacade.get_coordinates(params[:location])
-      
-      require 'pry'; binding.pry
+      WeatherFacade.get_forecast(location_coordinates)
       # forecast_data
     rescue
       render json: {"error" => {"message" => "Something went wrong"}}, status:400
