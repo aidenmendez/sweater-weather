301bcce9b619005e421a3de5cdd320f052f011d7
Aiden Mendez <aidenmendez@gmail.com>
2021-03-08 14:21:26 -0700

    Start background request spec

---
+++ spec/requests/api/v1/background_request_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+descibe 'Background image request' do
+  describe 'happy path' do
+    it 'returns an image given a location' do
+      location = 'denver,co'
+      get "/api/v1/backgrounds?location=#{location}"
+
+      data = JSON.parse(response.body)
+      expect(response)
+    end
+  end
+end
