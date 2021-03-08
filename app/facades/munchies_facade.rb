ce59556aac172eb4419158b18903fb0a94f9f4d5
Aiden Mendez <aidenmendez@gmail.com>
2021-03-08 10:15:04 -0700

    Create get forecast

--- app/facades/munchies_facade.rb
+++ app/facades/munchies_facade.rb
@@ -1,18 +1,31 @@
 class MunchiesFacade
   def initialize(start, destination, food)
-    @forecast = get_forecast(destination)
-    @destination_city = format_destination(destination)
-    @travel_time = get_travel_time(start, destination)
+    direction = Direction.new(start, destination)
+
+    @destination_city = direction.dest
+    @travel_time = direction.time
+    @forecast = get_forecast(destination) # make into a poro
+    @restaurant = Restaurant.new(@travel_time, destination, food)
   end
 
 
   private
+  # def format_destination(destination)
+  #   # format the destination or figure out where to pull it from
+  # end
+
+  # def get_travel_time(start, destination)
+  #   # call directions services
+  # end
+
+
   def get_forecast(destination)
     location_coordinates = GeocodeFacade.get_coordinates(destination)
     forecast = WeatherFacade.get_forecast(location_coordinates)
-    require 'pry'; binding.pry
-   data = {  summary: forecast.current_weather.conditions,
-            temperature: forecast.current_weather.temperature
-          }
+    
+    data = {  
+              :summary     => forecast.current_weather.conditions,
+              :temperature => forecast.current_weather.temperature
+            }
   end
 end
