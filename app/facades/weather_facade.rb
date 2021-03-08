f24f7f4e29d6a36b5d9c031d3b369e96742c2ebe
Aiden Mendez <aidenmendez@gmail.com>
2021-03-07 19:31:06 -0700

    Finish weather facade

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -2,7 +2,7 @@ class WeatherFacade
   class << self
     def get_forecast(location)
       weather_data = WeatherService.get_forecast(location)
-      Forecast.new(weather_data)
+      forecast = Forecast.new(weather_data)
     end
   end
 end
