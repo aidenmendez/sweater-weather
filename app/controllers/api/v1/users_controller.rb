864e9c1d248ce592e53bcd5066dc4c01d7024598
Aiden Mendez <aidenmendez@gmail.com>
2021-03-09 13:08:06 -0700

    Finish endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,33 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    require 'pry'; binding.pry
     #add sad path conditions later
-    user = User.create(params[:email])
-    require 'pry'; binding.pry
-    if user
-      render json: UserSerializer.new(user)
+    begin
+      params[:user] = {
+                        :email                  => params[:email], 
+                        :password               =>  params[:password],
+                        :confirmation_password  => params[:confirmation_password]
+                      }
+  
+      user = User.create!(user_params)
+      # render json: UserSerializer.new(user)
+      ser =  UserSerializer.new(user)
+      require 'pry'; binding.pry
+    rescue
+      require 'pry'; binding.pry
     end
+
+    # if 
+    #   
+    # else
+    #   # render an error
+    #   require 'pry'; binding.pry
+    # end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2021_03_09_175645) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.string "api_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
