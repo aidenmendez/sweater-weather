045869c47b4d44adcd8978335e10eb48141256ac
Aiden Mendez <aidenmendez@gmail.com>
2021-03-08 23:00:21 -0700

    Beef up background request test

--- spec/requests/api/v1/background_request_spec.rb
+++ spec/requests/api/v1/background_request_spec.rb
@@ -2,12 +2,19 @@ require 'rails_helper'
 
 describe 'Background image request' do
   describe '(happy path)' do
-    it 'returns an image given a location' do
+    it 'returns an image given a location', :vcr do
       location = 'denver,co'
       get "/api/v1/backgrounds?location=#{location}"
 
-      data = JSON.parse(response.body)
-      expect(response)
+      data = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(response.status).to eq(200)
+      expect(data[:id]).to be_nil
+      expect(data[:type]).to eq("image")
+      expect(data[:attributes][:location]).to eq(location)
+      expect(data[:attributes][:image_url]).to be_a(String)
+      expect(data[:attributes][:credit][:author]).to be_a(String)
+      expect(data[:attributes][:credit][:source]).to eq("www.unsplash.com")      
     end
   end
 end
