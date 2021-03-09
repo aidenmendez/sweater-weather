0e194450bccf497899d4b6f37b26fdd4aee80cc1
Aiden Mendez <aidenmendez@gmail.com>
2021-03-09 11:15:53 -0700

    Add bcrypt and start user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::UsersController < ApplicationController
     require 'pry'; binding.pry
     #add sad path conditions later
     user = User.create(params[:email])
-
+    require 'pry'; binding.pry
     if user
       render json: UserSerializer.new(user)
     end
