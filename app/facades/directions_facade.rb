2a70cab38af8ca129fdb95b94e534aa11f31209e
Aiden Mendez <aidenmendez@gmail.com>
2021-03-09 18:54:51 -0700

    Finish directions facade

--- app/facades/directions_facade.rb
+++ app/facades/directions_facade.rb
@@ -2,7 +2,12 @@ class DirectionsFacade
   class << self
     def get_directions(origin, destination)
       data = DirectionsService.get_data(origin, destination)
-
+      
+      { 
+        :start_city   => "#{data[:route][:locations][0][:adminArea5]}, #{data[:route][:locations][0][:adminArea3]}",
+        :end_city     => "#{data[:route][:locations][1][:adminArea5]}, #{data[:route][:locations][1][:adminArea3]}",
+        :travel_time  => data[:route][:realTime]
+       }
     end
   end
 end
