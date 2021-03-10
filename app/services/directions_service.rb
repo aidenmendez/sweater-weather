dadb61c3f097d04b5d6d45022ac8743f64bfeef4
Aiden Mendez <aidenmendez@gmail.com>
2021-03-09 18:54:23 -0700

    Finish directions service

---
+++ app/services/directions_service.rb
@@ -0,0 +1,24 @@
+class DirectionsService
+  class << self
+    def conn
+      Faraday.new("http://mapquestapi.com/directions/v2") do |req|
+        req.params['key'] = ENV['MAPQUEST_KEY']
+      end
+    end
+  
+    def get_data(beginning, destination)
+      response = conn.get('route') do |req|
+        req.params['from'] = beginning
+        req.params['to'] = destination
+      end
+      
+      JSON.parse(response.body, symbolize_names: true)
+  
+      # if data[:info][:statuscode] == 0
+      #   data
+      # else
+      #   {  }
+      # end      
+    end
+  end
+end
