8da3c775f6452f1f4c7b59981723bbe42a7dc44a
Aiden Mendez <aidenmendez@gmail.com>
2021-03-09 23:27:06 -0700

    Abstract road trip functionality out of controller into facade

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -3,11 +3,7 @@ class Api::V1::RoadTripController < ApplicationController
     user = User.find_by(api_key: params[:api_key])
     
     if user
-      direction_stats = DirectionsFacade.get_directions(params[:origin], params[:destination])
-      destination_coordinates = GeocodeFacade.get_coordinates(params[:destination])
-      weather_at_eta = WeatherFacade.get_short_forecast(destination_coordinates, direction_stats[:travel_time])
-      road_trip = RoadTrip.new(direction_stats[:start_city], direction_stats[:end_city], direction_stats[:travel_time], weather_at_eta)
-      
+      road_trip = RoadTripFacade.new_trip(params[:origin], params[:destination])
       render json: RoadTripSerializer.new(road_trip), status: 201
     else
       render json: {"error" => {"message" => "Unauthorized. Invalid or missing API key."}}, status:401
