8d2320c0f8217ea999b90bf3bec66f7c859f2905
Aiden Mendez <aidenmendez@gmail.com>
2021-03-09 18:32:12 -0700

    Call directions service from directions facade

--- spec/facades/api/v1/directions_facade_spec.rb
+++ spec/facades/api/v1/directions_facade_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 describe "Road Trip Facade" do
-  it "calls directions service and returns correct data" do
+  it "calls directions service and returns correct data", :vcr do
     origin = 'denver,co'
     destination = 'pueblo,co'
 
-    facade = RoadTripFacade.get_directions(origin, destination)
+    facade = DirectionsFacade.get_directions(origin, destination)
 
     expect(facade[:travel_time]).to be_an(Integer)
     expect(facade[:start_city]).to eq("Denver, CO")
