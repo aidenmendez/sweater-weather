ae569e85ee16dfa22ba73e1f4a8a9e039f31807e
Aiden Mendez <aidenmendez@gmail.com>
2021-03-09 23:11:26 -0700

    Add API key condition to road trip controller

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,17 +1,16 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-    # mapquest direction endpoint(pass start, end) => returns travel time (seconds), start city (formatted), end city (formatted)
-    direction_stats = DirectionsFacade.get_directions(params[:origin], params[:destination])
-    destination_coordinates = GeocodeFacade.get_coordinates(params[:destination])
-
-    # call weather_at_eta facade to get short forecast poro
-    weather_at_eta = WeatherFacade.get_short_forecast(destination_coordinates, direction_stats[:travel_time])
+    user = User.find_by(api_key: params[:api_key])
     
-    #create RoadTrip poro-- pass start_city, end_city, travel_time, weather_at_eta, 
-    road_trip = RoadTrip.new(direction_stats[:start_city], direction_stats[:end_city], direction_stats[:travel_time], weather_at_eta)
-    
-    render json: RoadTripSerializer.new(road_trip), status: 201
-
-    # send authentication error if key isn't provided
+    if user
+      direction_stats = DirectionsFacade.get_directions(params[:origin], params[:destination])
+      destination_coordinates = GeocodeFacade.get_coordinates(params[:destination])
+      weather_at_eta = WeatherFacade.get_short_forecast(destination_coordinates, direction_stats[:travel_time])
+      road_trip = RoadTrip.new(direction_stats[:start_city], direction_stats[:end_city], direction_stats[:travel_time], weather_at_eta)
+      
+      render json: RoadTripSerializer.new(road_trip), status: 201
+    else
+      render json: {"error" => {"message" => "Unauthorized. Invalid or missing API key."}}, status:401
+    end
   end
 end
