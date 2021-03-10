598a0503775447d4d1e17652bbb37e8a9824cd29
Aiden Mendez <aidenmendez@gmail.com>
2021-03-09 23:11:00 -0700

    Add tests for api key authorization

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -19,6 +19,7 @@ describe "Road trip request" do
 
       data = JSON.parse(response.body, symbolize_names: true)[:data]
 
+      expect(response.status).to eq(201)
       expect(data[:id]).to be_nil
       expect(data[:type]).to eq("roadtrip")
       expect(data[:attributes][:start_city]).to eq("Denver, CO")
@@ -30,6 +31,45 @@ describe "Road trip request" do
   end
 
   describe "(sad path)" do
+    it "should return a 401 response if API key is missing" do
+      user = User.create!({
+        :email                  => "whatever@example.com",
+        :password               => "password",
+        :password_confirmation  => "password"
+      })
+      
+      req_data = {
+        "origin": "Denver,CO",
+        "destination": "Pueblo,CO"
+      }
 
+      post "/api/v1/road_trip", :params => req_data
+
+      error = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(401)
+      expect(error[:error][:message]).to eq("Unauthorized. Invalid or missing API key.")
+    end
+
+    it "should return a 401 response if API key is incorrect" do
+      user = User.create!({
+        :email                  => "whatever@example.com",
+        :password               => "password",
+        :password_confirmation  => "password"
+      })
+      
+      req_data = {
+        "origin": "Denver,CO",
+        "destination": "Pueblo,CO",
+        "api_key": "fake_api_key"
+      }
+
+      post "/api/v1/road_trip", :params => req_data
+
+      error = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(401)
+      expect(error[:error][:message]).to eq("Unauthorized. Invalid or missing API key.")
+    end
   end
 end
