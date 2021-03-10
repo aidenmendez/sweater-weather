be279b0e8c756470f7b53ac29839494447a441cc
Aiden Mendez <aidenmendez@gmail.com>
2021-03-09 22:43:31 -0700

    Render json in controller -- tests passing

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -4,15 +4,13 @@ class Api::V1::RoadTripController < ApplicationController
     direction_stats = DirectionsFacade.get_directions(params[:origin], params[:destination])
     destination_coordinates = GeocodeFacade.get_coordinates(params[:destination])
 
-    # call weather_at_eta facade (pass end city and travel time)
+    # call weather_at_eta facade to get short forecast poro
     weather_at_eta = WeatherFacade.get_short_forecast(destination_coordinates, direction_stats[:travel_time])
-        # call weather service for hourly forecasts
-        # calculate arrival time
-        # select hourly forecast for arrival time
-        # create & return weather_at_eta poro
-
+    
     #create RoadTrip poro-- pass start_city, end_city, travel_time, weather_at_eta, 
-    # serialize
+    road_trip = RoadTrip.new(direction_stats[:start_city], direction_stats[:end_city], direction_stats[:travel_time], weather_at_eta)
+    
+    render json: RoadTripSerializer.new(road_trip), status: 201
 
     # send authentication error if key isn't provided
   end
