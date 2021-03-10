39dff7d5f0a1e9c0862f5618e124d08f0af6f4a1
Aiden Mendez <aidenmendez@gmail.com>
2021-03-09 22:41:43 -0700

    Add id nil to road trip

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -1,10 +1,12 @@
 class RoadTrip
-  attr_reader :start_city, 
+  attr_reader :id,
+              :start_city, 
               :end_city, 
               :travel_time, 
               :weather_at_eta
 
   def initialize(origin, destination, time, forecast)
+    @id = nil
     @start_city = origin
     @end_city = destination
     @travel_time = format_time(time)
