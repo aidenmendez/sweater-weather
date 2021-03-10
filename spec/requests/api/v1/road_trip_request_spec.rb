75ec490db4965d9448d26206b7f3b807b523c5ef
Aiden Mendez <aidenmendez@gmail.com>
2021-03-09 23:45:53 -0700

    Add testing for impossible destinations

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -71,5 +71,33 @@ describe "Road trip request" do
       expect(response.status).to eq(401)
       expect(error[:error][:message]).to eq("Unauthorized. Invalid or missing API key.")
     end
+
+    it "should return a travel time of impossible and weather should be empty", :vcr do
+      user = User.create!({
+        :email                  => "whatever@example.com",
+        :password               => "password",
+        :password_confirmation  => "password"
+      })
+      
+      req_data = {
+        "origin": "Denver,CO",
+        "destination": "Paris, France",
+        "api_key": user.api_key
+      }
+
+      post "/api/v1/road_trip", :params => req_data
+
+      data = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(response.status).to eq(201)
+      expect(data[:id]).to be_nil
+      expect(data[:type]).to eq("roadtrip")
+      expect(data[:attributes][:start_city]).to eq("Denver,CO")
+      expect(data[:attributes][:end_city]).to eq("Paris, France")
+      expect(data[:attributes][:travel_time]).to eq("impossible")
+      expect(data[:attributes][:weather_at_eta]).to be_a(Hash)
+      expect(data[:attributes][:weather_at_eta][:temperature]).to be_nil
+      expect(data[:attributes][:weather_at_eta][:conditions]).to be_nil
+    end
   end
 end
